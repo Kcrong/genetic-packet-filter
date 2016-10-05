a6b336162eee936b0557ae807cbbc00672c08691
kcrong <kcrong@kim82536.pe.kr>
2016-10-05 14:59:20 +0900

    페이로드 처리 예제 추가

--- example.py
+++ example.py
@@ -1,16 +1,22 @@
+# -*-coding; utf-8 -*-
 from pcapy import open_offline
 from impacket.ImpactDecoder import *
+from chardet import detect
 
 
-def handler(header, data):
+def handler(_, data):
     eth = EthDecoder().decode(data)
     ip = eth.child()
     proto = ip.child()
     payload = proto.get_data_as_string()
-    if payload == '':
-        return
-    else:
+
+    if payload is not None \
+            and payload != '' \
+            and detect(payload)['encoding'] == 'ascii':
         pass
+
+    else:
+        return None
     source_ip = ip.get_ip_src()
     dest_ip = ip.get_ip_dst()
     # print "Packet detected: %s -> %s" % (source_ip, dest_ip)
@@ -19,7 +25,7 @@ def handler(header, data):
 
 pcap = open_offline('test.pcap')
 
-pcap.setfilter('tcp')
-pcap.setfilter('dst port 80')
+pcap.setfilter('tcp ')
+pcap.setfilter('ip src not 8.8.8.8')
 
 pcap.loop(0, handler)
