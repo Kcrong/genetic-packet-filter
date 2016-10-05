001ac6288775b8a6701593fd6502633177b5d0e0
kcrong <kcrong@kim82536.pe.kr>
2016-10-05 12:27:17 +0900

    필터링 규칙 코드 추가 패킷 페이로드 출력 추가

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ def handler(header, data):
     eth = EthDecoder().decode(data)
     ip = eth.child()
     proto = ip.child()
+    payload = proto.get_data_as_string()
+    if payload == '':
+        return
+    else:
+        pass
     source_ip = ip.get_ip_src()
     dest_ip = ip.get_ip_dst()
-    print "Packet detected: %s -> %s" % (source_ip, dest_ip)
+    # print "Packet detected: %s -> %s" % (source_ip, dest_ip)
+    print payload
 
 
 pcap = open_offline('test.pcap')
 
-pcap.setfilter('tcp and port 80')
+pcap.setfilter('tcp')
+pcap.setfilter('dst port 80')
 
 pcap.loop(0, handler)
