e249f7a8a65a5c130914a2c6a699350c467ce991
kcrong <kcrong@kim82536.pe.kr>
2016-10-18 12:07:52 +0900

    Import util.timer Use timer at "run" func

---
+++ new.py
@@ -0,0 +1,27 @@
+"""
+We Need:
+    1. Packet Filtering --> Use set_filter() at Pcapy
+    2. Filtering Genetic --> Use Random Set. Feature will be IP, Port, Content
+    3. Show Graph --> Use
+"""
+from pcapy import open_offline
+from util import timer
+from impacket.ImpactDecoder import EthDecoder
+
+
+class Filter:
+    def __init__(self, rule):
+        self.rule = rule
+
+    @timer
+    def run(self, pcap):
+        def handler(_, data):
+            # Parsing Packet Data
+            eth = EthDecoder().decode(data)
+            ip = eth.child()
+            tcp = ip.child()
+            payload = tcp.get_data_as_string()
+
+        opener = open_offline(pcap)
+        opener.set_rule(self.rule)
+        pcap.loop(0, handler)
