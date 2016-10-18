48dbbfb97860f746477b9793d45844c43119b941
kcrong <kcrong@kim82536.pe.kr>
2016-10-18 12:40:35 +0900

    Set Attr that only not None

--- new.py
+++ new.py
@@ -10,6 +10,22 @@ from pcapy import open_offline
 from util import timer, counter
 
 
+class SetRule:
+    def __init__(self, ip=None, ip_active=None, port=None, port_active=None):
+        all_args = locals()
+        for key in all_args:
+
+            # Except self var
+            if key == 'self':
+                continue
+
+            # Set Attr only not None
+            args_data = all_args[key]
+
+            if args_data is not None:
+                setattr(self, key, args_data)
+
+
 class Filter:
     def __init__(self, rule):
         self.rule = rule
