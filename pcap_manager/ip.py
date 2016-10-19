bc9f853d62c37bd7f4099254dfd7d063c07178a4
kcrong <kcrong@kim82536.pe.kr>
2016-10-19 14:15:35 +0900

    Complete parse_all_ips Add comment Now return two list. all_src_ip, all_dst_ip

--- pcap_manager/ip.py
+++ pcap_manager/ip.py
@@ -4,11 +4,24 @@ from impacket.ImpactDecoder import EthDecoder
 
 
 def parse_all_ips(packet_filename):
-    all_ip = list()
+    """
+    :param packet_filename: pcap filename to parsing
+    :return: tuple ([all source ip list], [all destination ip list])
+    """
+    all_src_ip = list()
+    all_dst_ip = list()
 
     pcap_handler = open_offline(packet_filename)
-    count = Counter()
+    decoder = EthDecoder()
     while True:
-        print count
         _, data = pcap_handler.next()
 
+        if data is None:
+            break
+
+        ip = decoder.decode(data).child()  # same as Ethernet.child()
+
+        all_src_ip.append(ip.get_ip_src())
+        all_dst_ip.append(ip.get_ip_dst())
+
+    return all_src_ip, all_dst_ip
