0f7c32058b722bf1d9bb642b740fb10b66f13504
kcrong <kcrong@kim82536.pe.kr>
2016-10-22 19:41:03 +0900

    Add ignore when store ip,port,mac info

--- pcap_manager/total.py
+++ pcap_manager/total.py
@@ -1,6 +1,7 @@
 from pcapy import open_offline
 
 from utility.parser import PacketParser
+from utility.exception import ignore
 
 
 def parse_all_ip_port_mac(packet_filename):
@@ -21,11 +22,18 @@ def parse_all_ip_port_mac(packet_filename):
 
         p = PacketParser(pkt_data)
 
-        all_src_ip.add(p.src_ip)
-        all_dst_ip.add(p.dst_ip)
-        all_src_port.add(p.src_port)
-        all_dst_port.add(p.dst_port)
-        all_src_mac.add(p.src_mac)
-        all_dst_mac.add(p.dst_mac)
+        # What can i do..?
+        with ignore(AttributeError):
+            all_src_ip.add(p.src_ip)
+        with ignore(AttributeError):
+            all_dst_ip.add(p.dst_ip)
+        with ignore(AttributeError):
+            all_src_port.add(p.src_port)
+        with ignore(AttributeError):
+            all_dst_port.add(p.dst_port)
+        with ignore(AttributeError):
+            all_src_mac.add(p.src_mac)
+        with ignore(AttributeError):
+            all_dst_mac.add(p.dst_mac)
 
     return all_src_ip, all_dst_ip, all_src_port, all_dst_port, all_src_mac, all_dst_mac
