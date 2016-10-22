a67aa37bd21fa266539f13985536f769c74b7fd0
kcrong <kcrong@kim82536.pe.kr>
2016-10-22 12:55:51 +0900

    Edit import path example_code.poc

--- example_code/poc.py
+++ example_code/poc.py
@@ -10,7 +10,7 @@ from pcapy import open_offline
 
 from impacket.ImpactDecoder import EthDecoder
 
-from utility import counter, timer
+from utility.coverage import counter, timer
 
 AttackPacket = 'test.pcap'
 NormalPacket = 'test.pcap'
