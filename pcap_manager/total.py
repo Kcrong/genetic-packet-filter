127d9397bcb2c9ad437012fde7f36a28404eb2cc
kcrong <kcrong@kim82536.pe.kr>
2016-10-24 12:53:53 +0900

    Add multiple file processing at parse_all_ip_port_mac

--- pcap_manager/total.py
+++ pcap_manager/total.py
@@ -2,7 +2,7 @@ from pcapy import open_offline
 
 from utility.parser import PacketParser
 from utility.exception import ignore
-from utility.data_manage import return2type
+from utility.data_manage import return2type, is_iterable
 
 
 def remove_none(data):
@@ -11,30 +11,34 @@ def remove_none(data):
 
 @return2type(list)
 @return2type(remove_none)
-def parse_all_ip_port_mac(packet_filename):
+def parse_all_ip_port_mac(packet_files):
     all_src_ip = set()
     all_dst_ip = set()
     all_src_port = set()
     all_dst_port = set()
+    if is_iterable(packet_files) is False:
+        packet_files = list(packet_files)
 
-    pcap_handler = open_offline(packet_filename)
+    for packet_file in packet_files:
 
-    while True:
-        pkt_hdr, pkt_data = pcap_handler.next()
+        pcap_handler = open_offline(packet_file)
 
-        if pkt_hdr is None:
-            break
+        while True:
+            pkt_hdr, pkt_data = pcap_handler.next()
 
-        p = PacketParser(pkt_data)
+            if pkt_hdr is None:
+                break
 
-        # What can i do..?
-        with ignore(AttributeError):
-            all_src_ip.add(p.src_ip)
-        with ignore(AttributeError):
-            all_dst_ip.add(p.dst_ip)
-        with ignore(AttributeError):
-            all_src_port.add(p.src_port)
-        with ignore(AttributeError):
-            all_dst_port.add(p.dst_port)
+            p = PacketParser(pkt_data)
+
+            # What can i do..?
+            with ignore(AttributeError):
+                all_src_ip.add(p.src_ip)
+            with ignore(AttributeError):
+                all_dst_ip.add(p.dst_ip)
+            with ignore(AttributeError):
+                all_src_port.add(p.src_port)
+            with ignore(AttributeError):
+                all_dst_port.add(p.dst_port)
 
     return all_src_ip, all_dst_ip, all_src_port, all_dst_port
